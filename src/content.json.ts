39bcc4d70f80c148a13bafe53914ac77836a3a76
Agera Emmanuel <ageraemmanuel611@gmail.com>
2025-08-14 23:13:29 +0100

    Initial commit from Create Next App

---
+++ src/content.json.ts
@@ -0,0 +1,10 @@
+export const homeContent = [
+  {
+    imgUrl: `https://images.unsplash.com/photo-1632215861513-130b66fe97f4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8TmlnZXJpYW4lMjBzdHVkZW50fGVufDB8fDB8fHww`,
+    message: {
+      title: "Boast crediblit",
+      content: ``
+    } 
+
+  }
+]
